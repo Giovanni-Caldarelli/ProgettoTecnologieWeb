3c667e40677319cce889c41c1ba41bd3b9f435b1
Giovanni-Caldarelli <giovannicaldarelli4@gmail.com>
2025-02-14 15:50:28 +0100

    nuovi aggirornamenti

--- sitoWeb/prenotazione/salva_prenotazione.php
+++ sitoWeb/prenotazione/salva_prenotazione.php
@@ -18,18 +18,28 @@ $nome_parcheggio = $_POST['nome_parcheggio'];
 $data_prenotazione = $_POST['data_prenotazione'];
 $ora_prenotazione = $_POST['ora_prenotazione'];
 
-// Inseriamo la prenotazione nel database
-$query = "INSERT INTO prenotazioni (nome_utente, nome_parcheggio, data_prenotazione, ora_prenotazione) 
-          VALUES ($1, $2, $3, $4) RETURNING id";
-$result = pg_query_params($conn, $query, [$nome_utente, $nome_parcheggio, $data_prenotazione, $ora_prenotazione]);
+// Controlla se ci sono posti disponibili
+$query_posti = "SELECT posti_disponibili FROM parcheggi WHERE nome = $1";
+$result_posti = pg_query_params($conn, $query_posti, [$nome_parcheggio]);
+$row = pg_fetch_assoc($result_posti);
 
-if (!$result) {
-    die(json_encode(['success' => false, 'error' => pg_last_error($conn)]));
+if (!$row || $row['posti_disponibili'] <= 0) {
+    die(json_encode(['success' => false, 'error' => 'Posti esauriti o parcheggio inesistente']));
 }
 
-// Otteniamo l'ID della prenotazione appena creata
-$prenotazione = pg_fetch_assoc($result);
+// Inseriamo la prenotazione
+$query_prenotazione = "INSERT INTO prenotazioni (nome_utente, nome_parcheggio, data_prenotazione, ora_prenotazione) 
+                       VALUES ($1, $2, $3, $4)";
+$result_prenotazione = pg_query_params($conn, $query_prenotazione, [$nome_utente, $nome_parcheggio, $data_prenotazione, $ora_prenotazione]);
 
-// Rispondiamo con un JSON valido per il JavaScript
+if (!$result_prenotazione) {
+    die(json_encode(['success' => false, 'error' => 'Errore nel salvataggio della prenotazione']));
+}
+
+// Aggiorniamo il numero di posti disponibili
+$query_update = "UPDATE parcheggi SET posti_disponibili = posti_disponibili - 1 WHERE nome = $1";
+pg_query_params($conn, $query_update, [$nome_parcheggio]);
+
+// Reindirizziamo l'utente all'area riservata
 echo json_encode(['success' => true, 'redirect' => '../profilo/area_riservata.php']);
 ?>
